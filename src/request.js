cc60ff017c23c3030167a7125aed56bc8ee69035
samick17 <boneache@gmail.com>
2022-04-19 19:54:56 +0800

    1. Updated request.js with promise

--- src/request.js
+++ src/request.js
@@ -3,18 +3,25 @@ export default {
   request: () => {
     const id = 'test';
     const pwd = 'test';
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('readystatechange', (e) => {
-      if(xhr.readyState === XMLHttpRequest.DONE) {
-        console.log(xhr.status, xhr.response);
-      }
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.addEventListener('readystatechange', (e) => {
+        if(xhr.readyState === XMLHttpRequest.DONE) {
+          const status = xhr.status.toString();
+          if(status.startsWith('4') || status.startsWith('5')) {
+            reject(JSON.parse(xhr.response));
+          } else {
+            resolve(JSON.parse(xhr.response));
+          }
+        }
+      });
+      const payload = JSON.stringify({
+        id: id,
+        pwd: pwd,
+      });
+      xhr.open('POST', '/api/v1/signin');
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(payload);
     });
-    const payload = JSON.stringify({
-      id: id,
-      pwd: pwd,
-    });
-    xhr.open('POST', 'http://localhost:8000/api/v1/signin');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(payload);
   },
 };
